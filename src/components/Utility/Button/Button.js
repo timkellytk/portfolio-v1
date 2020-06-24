35dfa6af61451a9bed59d0e1fb12a0c87fe9ceb8
Tim Kelly <timpkelly1@gmail.com>
2020-06-24 13:10:18 +1000

    Create a Button utility for the arrow button

---
+++ src/components/Utility/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import arrow from '../../../assets/images/arrow-icon.svg';
+import style from './Button.module.css';
+
+const Button = (props) => {
+  return (
+    <a href={props.link} className={style.Button}>
+      <div>
+        <p className={style.ArrowText}>{props.children}</p>
+        <img src={arrow} className={style.ArrowIcon} alt="arrow icon" />
+      </div>
+    </a>
+  );
+};
+
+export default Button;
