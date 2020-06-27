4e5a306ed663c7ab7f1c4a1452d213c228446082
Tim Kelly <timpkelly1@gmail.com>
2020-06-27 17:09:31 +1000

    Created the basic contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import HelmetHOC from '../../hoc/Helmet/Helmet';
 import ogContact from '../../assets/images/openGraph/ogContact.png';
+import ContentContainer from '../Utility/ContentContainer/ContentContainer';
+import Animate from '../Utility/Animate/Animate';
+import style from './Contact.module.css';
+import github from '../../assets/images/utility/github.png';
+import linkedin from '../../assets/images/utility/linkedin.png';
+import instagram from '../../assets/images/utility/instagram.png';
 
 const Contact = (props) => {
   const SEO = (
@@ -14,7 +20,59 @@ const Contact = (props) => {
   return (
     <React.Fragment>
       {SEO}
-      <div>contact</div>
+      <ContentContainer>
+        <div className={style.CentreContent}>
+          <Animate delayTitle>
+            <div className={style.CentreContent}>
+              <div className={style.Avatar} />
+              <h1 className={style.Title}>Get in touch</h1>
+              <p>
+                I'm here to help you with your next website or web development
+                role. Get in touch at{' '}
+                <a href="mailto:timpkelly1@gmail.com">timpkelly1@gmail.com</a>.
+              </p>
+            </div>
+          </Animate>
+          <Animate delayContent>
+            <div className={style.ContactContainer}>
+              <a href="mailto:timpkelly@gmail.com" className={style.Button}>
+                Email Me
+              </a>
+              <div className={style.SocialContainer}>
+                <a
+                  href="https://github.com/timkellytk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={github} className={style.SocialIcon} alt="github" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/tim-patrick-kelly/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={linkedin}
+                    className={style.SocialIcon}
+                    alt="linkedin"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/thetimman/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={instagram}
+                    className={style.SocialIcon}
+                    alt="instagram"
+                  />
+                </a>
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </ContentContainer>
     </React.Fragment>
   );
 };
