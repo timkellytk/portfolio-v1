2d87a5ae4937051d7b5401eee1748d8f81e292ed
Tim Kelly <timpkelly1@gmail.com>
2020-07-06 08:15:55 +1000

    Installed and implemented react-router-sitemap and dependencies

---
+++ sitemap-routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+	<Route>
+		<Route path="/about" />
+		<Route path="/portfolio" />
+		<Route path="/portfolio/evolutionary-tutors" />
+		<Route path="/portfolio/the-rambler-co" />
+		<Route path="/contact" />
+	</Route>
+);
