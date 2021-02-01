e7a9a90aa833c429579038ac7bf80c8939b74242
Tim Kelly <timpkelly1@gmail.com>
2021-02-02 08:10:39 +1100

    Added the waldo proejct

--- src/components/Portfolio/SketchboxDesign/SketchboxDesignProject/SketchboxDesignProject.js
+++ src/components/Portfolio/SketchboxDesign/SketchboxDesignProject/SketchboxDesignProject.js
@@ -2,8 +2,7 @@ import React from 'react';
 import HelmetHOC from '../../../../hoc/Helmet/Helmet';
 import PortfolioProject from '../../../../hoc/PortfolioProject/PortfolioProject';
 import ogEvolutionary from '../../../../assets/images/openGraph/ogEvolutionary.png';
-import avatar from '../../../../assets/images/projects/evolutionary-tutors/darren-pehn.jpeg';
-import logo from '../../../../assets/images/projects/evolutionary-tutors/evolutionary-logo.png';
+import logo from '../../../../assets/images/projects/sketchbox-design/sketchbox-design-logo.png';
 import home from '../../../../assets/images/projects/evolutionary-tutors/screenshots/home.png';
 import infoCards from '../../../../assets/images/projects/evolutionary-tutors/screenshots/info-cards.png';
 import tickpoints from '../../../../assets/images/projects/evolutionary-tutors/screenshots/tickpoints.png';
@@ -33,18 +32,13 @@ const EvolutionaryTutorsProject = () => {
     <React.Fragment>
       {SEO}
       <PortfolioProject
-        name="Evolutionary Tutors"
+        name="Sketchbox Design"
         logo={logo}
         url="https://www.evolutionarytutors.com.au/"
-        projectTitle="Website Build - Sketchbox Design"
-        projectDescription="A bespoke website to find students the best tutors, give tutors
-              effective training and save administrators time."
+        projectTitle="Website Rebrand"
+        projectDescription="A website update to implement the new brand guidleines, remove all CSS bugs and create a cleaner user experience."
         gradient1="#d3d3f9"
         gradient2="#c8f4fc"
-        avatar={avatar}
-        quote="Seldom do you come across such a professional and competent young person like Tim. My partner and I had the pleasure of dealing with Tim and Nathan for over six weeks during our acquisition of Evolutionary Tutors. We were particularly impressed by Tim's transparency and attention to detail throughout the entire transaction and, in particular, the handover process. His responses to our queries were always timely, detailed and well-considered."
-        avatarName="Darren Pehn"
-        avatarTitle="Co-Owner of Evolutionary Tutors"
         images={[
           home,
           sydney,
