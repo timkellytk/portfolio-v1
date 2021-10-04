1aaece3ddaa1a56d6ef65f852ff8c670e9e5b121
Tim Kelly <timpkelly1@gmail.com>
2021-10-04 11:18:18 +1100

    Updated timkelly.dev to https://timkellytk.github.io/portfolio/ for new deployment

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,16 +1,14 @@
 require("babel-register")({
-	presets: ["es2015", "react"]
+  presets: ["es2015", "react"],
 });
 
 const router = require("./sitemap-routes").default;
 const Sitemap = require("react-router-sitemap").default;
 
 function generateSitemap() {
-	return (
-		new Sitemap(router)
-			.build("https://timkelly.dev/")
-			.save("./public/sitemap.xml")
-	);
+  return new Sitemap(router)
+    .build("https://timkellytk.github.io/portfolio/")
+    .save("./public/sitemap.xml");
 }
 
 generateSitemap();
